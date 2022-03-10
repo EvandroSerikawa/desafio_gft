5c1a9f6d4597041ccdaaa458291a3fdc24aa17ca
Evandro <serikawabass@hotmail.com>
2022-03-09 23:54:28 -0300

    Initial commit

---
+++ src/main/java/one/digitalInnovation/desafio_gft/DesafioGftApplication.java
@@ -0,0 +1,15 @@
+package one.digitalInnovation.desafio_gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class DesafioGftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGftApplication.class, args);
+	}
+
+}
